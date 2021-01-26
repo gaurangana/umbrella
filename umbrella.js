685e7171132cfc94b87f8934fc43360f1a78ba36
gaurangana <74183423+gaurangana@users.noreply.github.com>
2021-01-26 21:17:58 +0530

    Update umbrella.js

--- umbrella.js
+++ umbrella.js
@@ -6,7 +6,7 @@ class Umbrella{
            isStatic: true ,
 
        }
-       this.image= loadAnimation("sprites/walking_1.png, sprites/walking_2.png, sprites/walking_3.png, sprites/walking_4.png, sprites/walking_5.png, sprites/walking_6.png, sprites/walking_7.png, sprites/walking_8.png,")
+       this.image= loadAnimation("sprites/walking_1.png, sprites/walking_2.png, sprites/walking_3.png, sprites/walking_4.png, sprites/walking_5.png, sprites/walking_6.png, sprites/walking_7.png, sprites/walking_8.png")
        this.image.frameDelay= 20 ;
 
        this.umbrella = Bodies.circle(x,y,50,option) ;
